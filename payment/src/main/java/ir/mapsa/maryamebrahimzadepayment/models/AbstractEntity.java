2ef9639a534c42986e3ba9f07c5d3fdb9c276abb
maryamebrahimzade <ma.ebrahimzadeh@gmail.com>
2023-07-10 22:57:12 +0330

    edit->payment

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/models/AbstractEntity.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/models/AbstractEntity.java
@@ -2,9 +2,7 @@ package ir.mapsa.maryamebrahimzadepayment.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
